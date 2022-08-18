3c6c41f93fc46973d9da5535253ac3973586bd44
bala17o4 <60806902+bala17o4@users.noreply.github.com>
2022-08-18 13:19:39 +0530

    Update trial.py

--- trial.py
+++ trial.py
@@ -1,19 +1,44 @@
+from logging import Manager
 from os import name
 from flask import *
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from webdriver_manager.chrome import ChromeDriverManager
+from webdriver_manager.firefox import GeckoDriverManager
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.common.by import By
 
 app = Flask(__name__)
 
 
-@app.route('/login', methods = ['POST','GET'])
-def login():
-    return render_template('login.html')
-
-@app.route('/submit')
-def about():
+@app.route('/focus', methods=['POST','GET'])
+def whatsapp_web():
     if request.method=='POST':
         data = request.form
-        return render_template("submit.html",data = data)
+        brow=request.form.get('browser')
+        if(brow=='chrome'):
+            c_options = Options()
+            c_options.add_experimental_option("detach",True)
+            s=Service(ChromeDriverManager().install())
+            driver = webdriver.Chrome(service=s, chrome_options=c_options)
+            driver.maximize_window()
+            driver.get(request.form.get('link'))
+            # textDemo = driver.find_element("xpath","// a[contains(text(),\'sign')]").click()
+            search_text = "signin"
+            source = driver.page_source
+            if(search_text in source):
+                search_text.click()
+                print("true")
 
+            else:
+                print("false")
+            
+        if(brow=='firefox'):
+            s=Service(GeckoDriverManager().install())
+            driver = webdriver.Firefox(service=s)
+            driver.maximize_window()
+            driver.get("https://web.whatsapp.com/")
+        return render_template('submit.html',result = data)
 
 
 
