b3313a8df60dac836067692f8f2f65bef38e9f51
bala17o4 <60806902+bala17o4@users.noreply.github.com>
2023-03-19 17:37:12 +0530

    Add files via upload

--- trial.py
+++ trial.py
@@ -1,12 +1,15 @@
 from logging import Manager
 from os import name
 from flask import *
+from helium import *
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
+import chromedriver_autoinstaller as chromedriver
 from webdriver_manager.chrome import ChromeDriverManager
 from webdriver_manager.firefox import GeckoDriverManager
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.by import By
+import time
 
 app = Flask(__name__)
 
@@ -19,19 +22,35 @@ def whatsapp_web():
         if(brow=='chrome'):
             c_options = Options()
             c_options.add_experimental_option("detach",True)
-            s=Service(ChromeDriverManager().install())
-            driver = webdriver.Chrome(service=s, chrome_options=c_options)
-            driver.maximize_window()
-            driver.get(request.form.get('link'))
-            # textDemo = driver.find_element("xpath","// a[contains(text(),\'sign')]").click()
-            search_text = "signin"
-            source = driver.page_source
-            if(search_text in source):
-                search_text.click()
-                print("true")
+            c_options.add_argument("start-maximized")
+            c_options.add_experimental_option("excludeSwitches", ["enable-automation"])
+            c_options.add_experimental_option('excludeSwitches', ['enable-logging'])
+            c_options.add_experimental_option('useAutomationExtension', False)
+            c_options.add_argument('--disable-blink-features=AutomationControlled')
+            s=Service(chromedriver.install())
+            driver = start_chrome(request.form.get('link'),options=c_options)
+            time.sleep(10)
+            try:
+                click('sign in')
+            except LookupError:
+                try:
+                    click('log in')
+                except LookupError:
+                    pass
 
-            else:
-                print("false")
+
+            # driver = webdriver.Chrome(service=s, chrome_options=c_options)
+            # driver.maximize_window()
+            # driver.get(request.form.get('link'))
+            # textDemo = driver.find_element("xpath","// a[contains(text(),\'sign')]").click()
+            # search_text = "signin"
+            # source = driver.page_source
+            # if(search_text in source):
+            #     search_text.click()
+            #     print("true")
+
+            # else:
+            #     print("false")
             
         if(brow=='firefox'):
             s=Service(GeckoDriverManager().install())
@@ -49,3 +68,13 @@ def intro():
 
 if __name__ == '__main__':
     app.run(debug=True)
+
+
+
+
+
+
+
+
+
+
